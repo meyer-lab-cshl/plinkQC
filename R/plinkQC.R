7c1f3674250ad70659780d36e2ba19a843926a59
HannahVMeyer <hannah.v.meyer@googlemail.com>
2018-10-05 10:57:00 +0100

    import, importFrom File

---
+++ R/plinkQC.R
@@ -0,0 +1,12 @@
+#' plinkQC
+#'
+#' @docType package
+#' @name plinkQC
+#' @import ggplot2 
+#' @importFrom methods getPackageName as
+#' @importFrom utils read.table write.table 
+#' @importFrom optparse make_option parse_args OptionParser
+#' @importFrom dplyr filter select
+#' @importFrom cowplot plot_grid
+#' @importFrom UpSetR upset
+NULL
