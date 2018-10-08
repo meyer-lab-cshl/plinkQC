3dfcf5152330f045b7dcaf90be51980d0c3519fc
HannahVMeyer <hannah.v.meyer@googlemail.com>
2018-10-08 12:24:02 +0100

    Add combn as import

--- R/plinkQC.R
+++ R/plinkQC.R
@@ -4,7 +4,7 @@
 #' @name plinkQC
 #' @import ggplot2
 #' @importFrom methods getPackageName as
-#' @importFrom utils read.table write.table
+#' @importFrom utils read.table write.table combn
 #' @importFrom optparse make_option parse_args OptionParser
 #' @importFrom dplyr filter select
 #' @importFrom cowplot plot_grid
