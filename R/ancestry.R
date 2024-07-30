10f80c83acdb1918288f039f6a76a35fbd4b835c
Maha Syed <syed@cshl.edu>
2024-07-30 13:39:15 -0400

    Adding a script for ancestry classificatioin

---
+++ R/ancestry.R
@@ -0,0 +1,20 @@
+#' Identifying ancestries of samples from plink data samples
+#' 
+#' 
+#' 
+
+#' function 1 - helper function that turns plink1 into plink2 format
+#' 
+#'  
+
+
+#' function 2, projects data on old PC space (will have files from 1000 Genome)
+#'  #checks if the input is a bfile or pfile, if bfile, 
+#'        print out little note sayinbg converting to plink2 formation - 
+#'              put in a temp directory
+#'        goes to helper function 1
+#'  then:
+#'  #otherwise, loads RF 
+#'  #predicts with it and returns output of sample and labels
+#'  
+
