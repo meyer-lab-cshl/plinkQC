aee492fc304902f1000a57c5bf399ad774da0955
Maha Syed <syed@cshl.edu>
2024-09-03 17:10:54 -0400

    files for testing and vignettes

--- vignettes/AncestryCheck.R
+++ vignettes/AncestryCheck.R
@@ -3,26 +3,3 @@ knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
-
-## ----check ancestry, eval=FALSE, fig.height=3, fig.width=5, fig.align='center'----
-#  library(plinkQC)
-#  indir <- system.file("extdata", package="plinkQC")
-#  qcdir <- "~/qcdir"
-#  name <- 'data'
-#  refname <- 'all_hg38'
-#  prefixMergedDataset <- paste(name, ".", refname, sep="")
-#  
-#  exclude_ancestry <-
-#      evaluate_check_ancestry(indir=qcdir, name=name,
-#                              prefixMergedDataset=prefixMergedDataset,
-#                              refSamplesFile=paste(indir,
-#                                                   "/Genomes1000_ID2Pop.txt",
-#                                                   sep=""),
-#                              refColorsFile=paste(indir,
-#                                                  "/Genomes1000_PopColors.txt",
-#                                                  sep=""),
-#                              interactive=TRUE)
-
-## ----load ancestry, out.width = "500px", echo=FALSE, fig.align='center'-------
-knitr::include_graphics("checkAncestry.png")
-
